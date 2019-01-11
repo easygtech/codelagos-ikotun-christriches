d03c47631e65113680e1dcd814fadbc99a2fe91f
easygtech <46527388+easygtech@users.noreply.github.com>
2019-01-11 10:33:02 +0100

    Add files via upload

---
+++ python/My Code Lagos Project.py
@@ -0,0 +1,101 @@
+# General Knowledge Quiz
+
+import time
+
+score=int()
+print("Welcome to the General Knowledge Questions Quiz")
+time.sleep(2)
+print("Question 1")
+time.sleep(2)
+Question1 = input("When did Nigeria gain her independence?")
+if Question1 ==("1960") or Question1 ==("1960"):
+    print("Correct answer")
+    score = score + 1
+else:
+    print("Incorrect answer, the correct answer is 1960")
+time.sleep(2)
+print("Question 2")
+time.sleep
+Question2 = input("Who is the president of Nigeria?")
+if Question2 ==("Mohamadu Buhari") or Question2 ==("Mohamadu Buhari"):
+    print("Correct answer")
+    score = score + 1
+else:
+    print("Incorrect answer, the correct answer is Mohamadu Buhari")
+time.sleep(2)
+print("Question 3")
+time.sleep
+Question3 = input("How many hours are there in a leap year? ")
+if Question3 == ("8784") or Question3 ==("8784"):
+    print("Correct answer")
+    score = score + 1
+else:
+    print("Incorrect answer, the correct answer is 8784")
+time.sleep(2)
+print("Question 4")
+time.sleep(2)
+Question4 = input("Germany is what continent?")
+if Question4 == ("Europe") or Question4 ==("Europe"):
+    print("Correct answer")
+    score = score + 1
+else:
+    print("Incorrect answer, the correct answer is Europe")
+time.sleep(2)
+print("Question 5")
+time.sleep(2)
+Question5 = input("What is the capital of Nigeria? ")
+if Question5 == ("Abuja") or Question5 == ("Abuja"):
+    print("Correct answer")
+    score = score + 1
+else:
+    print("Incorrect answer, the correct answer is Abuja")
+time.sleep(2)
+print("Question 6")
+time.sleep(2)
+Question6 = input("In which year did Theresa May become Prime Minister? ")
+if  Question6 == ("2016") or Question6 == ("2016"):
+    print("Correct answer")
+    score = score + 1
+else:
+    print("Incorrect answer, the correct answer is 2016")
+time.sleep(2)
+print("Question 7")
+time.sleep(2)
+Question7 = input("In the game of football, how many players make a team? ")
+if Question7 == ("Twelve players") or Question7 == ("Twelve players"):
+    print("Correct answer")
+    score = score + 1
+else:
+    print("Incorrect answer, the correct answer is Twelve players")
+time.sleep(2)
+print("Question 8")
+time.sleep(2)
+Question8 = input("Nigeria consists of how many States? ")
+if Question8 == ("36 States") or Question8 == ("36 States"):
+    print("Correct answer")
+    score = score + 1
+else:
+    print("Incorrect answer, the correct answer is 36 States")
+time.sleep(2)
+print("Question 9")
+time.sleep(2)
+Question9 = input("Who is the presidential aspirant of PDP in Nigeria? ")
+if Question9 == ("Atiku Abubakar") or Question9 == ("Atiku Abubakar"):
+    print("Correct answer")
+    score = score + 1
+else:
+    print("Incorrect answer, the correct answer is Atiku Abubakar")
+time.sleep(2)
+print("Question 10")
+time.sleep(2)
+Question10 = input("Which type of angle is greater than 90 degrees but less than 180 degrees? ")
+if Question10 == ("Obtuse") or ("Obtuse"):
+    print("Correct answer")
+    score = score + 1
+time.sleep(2)
+print("This is the end of the quiz")
+time.sleep(1)
+print("You got",(score),"/ 10")
+num1 = (score)
+num2 = 10
+print('{0:.2f}%'.format((num1 / num2*100)))
